8b6d3fec3f0f92c187f2d417337b5085d7b2793d
OlejaCat <118455092+OlejaCat@users.noreply.github.com>
2023-02-16 14:59:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
 let btn = document.getElementById("btn");
-let username = document.getElementById("username");
-let password = document.getElementById("password");
 
 btn.addEventListener(
     "click", function() {
+        tg.MainButton.setText("Сообщение отправлено");
+        tg.MainButton.show();
         tg.sendData("testtest");
     }
- )
+ );
